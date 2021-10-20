daec2b510a586673264e2ebdad367255285d1526
Ashraff Hatz <ashraff.hatz@gmail.com>
2021-10-20 01:31:44 +0100

    config update

--- ms_app/settings.py
+++ ms_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'qprh##z+bk%_0=8hu-9gzudeckvett8+25ry@9m2scc_f#3(_0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','localhost', 'heroku']
 
 
 # Application definition
@@ -125,4 +126,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
