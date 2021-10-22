5092f8cf765152d738c0126f1e9991746b3635f4
Ashraff Hatz <ashraff.hatz@gmail.com>
2021-10-22 05:27:18 +0100

    purchases refactor

--- purchases_app/models.py
+++ purchases_app/models.py
@@ -5,14 +5,16 @@ from sales.models import Sales
 class Purchases(models.Model):
 
   purchase_code = models.CharField(primary_key=True,verbose_name="Purchase Order Number",max_length=10)
+  project_code = models.ForeignKey(Sales, on_delete=models.NULL)
   po_date = models.DateField()
   supplier_name	= models.CharField(max_length=100)
   purchased_items = models.CharField(max_length=80)
   value = models.DecimalField(max_digits=8, decimal_places=2)
   currency = models.CharField(max_length=5, choices=Currency.choices, default=Currency.MYR)
-  project_code = models.ForeignKey(Sales, on_delete=models.NULL)
   expected_date = models.DateField(verbose_name="Expected Paymenet Date")
   supplier_date = models.DateField(verbose_name="Supplier Delivary Date")
+
+  cancelled = models.BooleanField(default=False)
 
   @property
   def amount(self):
