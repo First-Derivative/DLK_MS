229172f5b43c4f4868faad488d4dbbe4c02fda83
Ashraff Hatz <ashraff.hatz@gmail.com>
2021-10-22 03:59:50 +0100

    build login page UI v0.1

--- users/forms.py
+++ users/forms.py
@@ -11,13 +11,15 @@ class LoginForm(ModelForm):
     fields = ("email", "password")
 
   email = forms.CharField(max_length=50,widget=forms.TextInput(attrs={
-    "class": "form-control std_input",
+    "class": "form-control login_input",
     "id": "login_email",
+    "placeholder": "email"
   }))
 
   password = forms.CharField(widget=forms.PasswordInput(attrs={
-    "class": "form-control std_input",
+    "class": "form-control login_input",
     "id": "login_password",
+    "placeholder": "password"
   }))
 
   # field cleaning and login auth
