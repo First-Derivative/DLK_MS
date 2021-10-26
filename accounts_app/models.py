2fcf6ded19f8ebaa5736040bc5ec28f54f7c6a7f
Ashraff Hatz <ashraff.hatz@gmail.com>
2021-10-26 01:02:18 +0100

    update model with ResolveCurrency auxiliary function

--- accounts_app/models.py
+++ accounts_app/models.py
@@ -4,3 +4,9 @@ class Currency(models.TextChoices):
   MYR = ("MYR","RM")
   EUR = ("EUR","€")
   USD = ("USD","$")
+
+#Auxiliary Function to resolve object currency value for serialization
+def resolveCurrency(currency):
+  for choice in Currency:
+    if(choice == currency):
+      return choice.label
