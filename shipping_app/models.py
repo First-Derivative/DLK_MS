490887334875a07249d9f8f85089fbc8ba128b94
Ashraff Hatz <ashraff.hatz@gmail.com>
2021-10-26 03:16:33 +0100

    implement add and get request handlers for Shipping

--- shipping_app/models.py
+++ shipping_app/models.py
@@ -2,7 +2,8 @@ from django.db import models
 from sales_app.models import Sales
 
 class Shipping(models.Model):
-  project_code = models.ForeignKey(Sales, on_delete=models.PROTECT)
+  # shipping_id = models.BigAutoField(primary_key=True)
+  project_code = models.CharField(max_length=10)
   project_name = models.CharField(max_length=80)
   client_name = models.CharField(max_length=100)
   # germany = models.BooleanField(default=False) Double Check with Mum on purpose
--- shipping_app/views.py
+++ shipping_app/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from ms_app.decorators import *
+from .models import Shipping
 
-# Create your views here.
+def serializeShipping(shipment):
+  serial = {}
+  serial["project_code"] = shipment.project_code
+  serial["project_name"] = shipment.project_name
+  serial["client_name"] = shipment.client_name
+  serial["customer"] = shipment.customer
+  serial["status"] = shipment.status
+  serial["remark"] = shipment.remark
+  serial["cancelled"] = shipment.cancelled
+
+@method_check(allowed_methods=["GET"])
+@unauthenticated_check
+def getShipping(request):
+  unserialized_shipping = Shipping.objects.all()
+  shipping = []
+  for shipment in unserialized_shipping:
+    shipping.append(serializeShipping(shipment))
+
+@method_check(allowed_methods=["POST"])
+@role_check(allowed_roles=["shipping"])
+def addShipping(request):
+  post = request.POST
+
+  # Validate postdata for duplication 
+  try:
+    shipping = Shipping.objects.get(project_code=post["data[project_code]"])
+    return JsonResponse({"error":"Shipping order with that shipping code already exists, please check for duplicate records"})
+  # No Duplicate Found-> Create new Object
+  except Shipping.DoesNotExist:
+    project_code = post["data[project_code]"]
+    project_name = post["data[project_name]"]
+    client_name = post["data[client_name]"]
+    customer = post["data[customer]"]
+    status = post["data[status]"]
+    remark = post["data[remark]"]
+
+    # Instantiate and save new Shipping object on DB
+    new_shipping = Shipping(project_code=project_code, project_name=project_name, client_name=client_name, customer=customer, status=status, remark=remark, cancelled=False)
+    #end of user-flow for succesful request: return status OK
+    return JsonResponse({"status":"OK"})
