111155f084f04d78d5d824802a68fbcb64c68dbf
Ashraff Hatz <ashraff.hatz@gmail.com>
2022-01-28 02:02:29 +0000

    WIP update accounts

--- accounts_app/views.py
+++ accounts_app/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import render
 from .models import PaymentStatus
 from ms_app.decorators import *
@@ -5,5 +6,33 @@ from ms_app.decorators import *
 @unauthenticated_check
 @method_check(allowed_methods=['GET'])
 def getAccountsPage(request):
-  context = { "accounts": PaymentStatus.objects.all() }
+  context = { "accounts": "" }
+  if(request.GET.get('search')):
+    search = request.GET.get('search')
+    results = {}
+    invoice_date_query = {}
+
+    # Search all fields for search value
+    invoice_number_query = PaymentStatus.objects.filter(invoice_number = search)
+    try:
+      invoice_date_query = PaymentStatus.objects.filter(invoice_date = search)
+    except ValidationError:
+      pass
+    status_query = PaymentStatus.objects.filter(status = search)
+    
+    for entry in invoice_number_query:
+      results[entry.paymentstatus_id] = entry
+    for entry in invoice_date_query:
+      results[entry.paymentstatus_id] = entry
+    for entry in status_query:
+      results[entry.paymentstatus_id] = entry
+    
+    # Remove duplicates from results
+    print(results)
+    context["accounts"] = list(results.values())
+    context["search"] = True
+  else:
+    context["accounts"] = PaymentStatus.objects.all() 
+    
+    
   return render(request, "accounts_app/accounts.html", context)
