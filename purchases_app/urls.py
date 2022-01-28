ff0e8159de1d7a2b7d52825a81a519aeb0aa783b
Ashraff Hatz <ashraff.hatz@gmail.com>
2022-01-28 02:02:11 +0000

    Update purchases_app urls typo for search

--- purchases_app/urls.py
+++ purchases_app/urls.py
@@ -4,7 +4,7 @@ from .views import *
 urlpatterns = [
   path("", purchasesPage, name="purchases"),
   path("api/getAll", getAllPurchases, name="getAllPurchases_API"),
-  path("api/serach", searchAPI.as_view(), name="searchPurchases_API"),
+  path("api/search", searchAPI.as_view(), name="searchPurchases_API"),
   path("api/post/new", postNewPurchases, name="postNewPurchases_API"),
   path("api/post/edit", postEditPurchases, name="postEditPurchases_API")
 ]
